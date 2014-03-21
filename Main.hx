37e80d31968878e888a80cd48c750d43ce2e8e76
dimanux <dimanux@gmail.com>
2014-03-21 16:17:35 +0400

    Loader test

--- Main.hx
+++ Main.hx
@@ -1,6 +1,19 @@
+import flash.display.Loader;
+import flash.events.Event;
+import flash.net.URLRequest;
+
 class Main
 {	
 	public static function main() 
 	{
+		for (i in 1...81)
+		{
+			var loader = new Loader();
+			var request = new URLRequest("assets/2 (" + i + ").png");
+			loader.contentLoaderInfo.addEventListener(Event.COMPLETE, function(e : Event) {
+				trace("Loaded " + request.url);
+			});
+			loader.load(request);
+		}
 	}
 }
